67943473316ee87c2daf42579de8558f6f0501d7
mickjoshan <vipinpandey3510@gmail.com>
2025-12-10 09:29:09 -0800

    seven

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './.env'
-})
+    path: "./.env"
+});
 
+// 1️⃣ CREATE EXPRESS APP  ✔
+const app = express();
+
+// 2️⃣ MIDDLEWARE
+app.use(express.json());
+
+// 3️⃣ CONNECT DB & START SERVER
 connectDB()
-.then(() => {
+  .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
-})
+  });
